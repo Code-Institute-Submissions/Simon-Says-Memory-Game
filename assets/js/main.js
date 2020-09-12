1662fe44cab3367ef83cc8e7b4af1408d291989f
MichaelWarner32 <warner.mike32@gmail.com>
2020-09-12 21:56:52 +0000

    Tested if the colors flash. They do and the correct audio sounds work at the same time. Had an issue where the colours would change position after one colour had flashed. Fixed it as I forgot to change the colour back to it's original state after a flash.

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,98 +28,101 @@ harshButton.addEventListener('click', (event) => {
 });
 
 onButton.addEventListener('click', (event) => {
-    if (onButton.checked == true) {
-        on - true;
-        turnCounter.innerHTML = "--";
-    } else {
-        on = false;
-        turnCounter.innerHTML = "";
-        clearColor();
-        clearInterval(intervalId);
-    }
+  if (onButton.checked == true) {
+    on = true;
+    turnCounter.innerHTML = "--";
+  } else {
+    on = false;
+    turnCounter.innerHTML = "";
+    clearColor();
+    clearInterval(intervalId);
+  }
 });
 
 startButton.addEventListener('click', (event) => {
-    if (on || win) {
-        play();
-    }
+  if (on || win) {
+    play();
+  }
 });
 
 function play() {
-    win = false;
-    order = [];
-    playerOrder = [];
-    flash = 0;
-    intervalId = 0;
-    turn = 1;
-    turnCounter.innerHTML = 1;
-    good = true;
-    for (var i = 0; i < 20; i++) {
-        order.push(math.floor(math.random() * 4) + 1);
-    }
-    compTurn = true;
+  win = false;
+  order = [];
+  playerOrder = [];
+  flash = 0;
+  intervalId = 0;
+  turn = 1;
+  turnCounter.innerHTML = 1;
+  good = true;
+  for (var i = 0; i < 20; i++) {
+    order.push(Math.floor(Math.random() * 4) + 1);
+  }
+  compTurn = true;
 
-    intervalId = setInterval(gameTurn, 800);
+  intervalId = setInterval(gameTurn, 800);
 }
 
 function gameTurn() {
-    on = false;
+  on = false;
 
-    if (flash == turn) {
-        clearInterval(intervalId);
-        compTurn = false;
-        clearColor();
-        on = true;
-    }
+  if (flash == turn) {
+    clearInterval(intervalId);
+    compTurn = false;
+    clearColor();
+    on = true;
+  }
 
-    if (compTurn) {
-        clearColor();
-        setTimeout(() => {
-            if (order[flash] == 1) one();
-            if (order[flash] == 2) two();
-            if (order[flash] == 3) three();
-            if (order[flash] == 4) four();
-            flash++;
-        }, 200);
-    }
+  if (compTurn) {
+    clearColor();
+    setTimeout(() => {
+      if (order[flash] == 1) one();
+      if (order[flash] == 2) two();
+      if (order[flash] == 3) three();
+      if (order[flash] == 4) four();
+      flash++;
+    }, 200);
+  }
 }
 
 function one() {
-    if (noise) {
-        let audio = document.getElementById("clip1");
-        audio.play();
-    }
-    noise = true;
-    bluePanel.style.backgroundColor = "lightskyblue";
+  if (noise) {
+    let audio = document.getElementById("clip1");
+    audio.play();
+  }
+  noise = true;
+  bluePanel.style.backgroundColor = "lightskyblue";
 }
+
 function two() {
-    if (noise) {
-        let audio = document.getElementById("clip2");
-        audio.play();
-    }
-    noise = true;
-    greenPanel.style.backgroundColor = "lightgreen";
+  if (noise) {
+    let audio = document.getElementById("clip2");
+    audio.play();
+  }
+  noise = true;
+  greenPanel.style.backgroundColor = "lightgreen";
 }
+
 function three() {
-    if (noise) {
-        let audio = document.getElementById("clip3");
-        audio.play();
-    }
-    noise = true;
-    redPanel.style.backgroundColor = "tomato";
+  if (noise) {
+    let audio = document.getElementById("clip3");
+    audio.play();
+  }
+  noise = true;
+  redPanel.style.backgroundColor = "tomato";
 }
+
 function four() {
-    if (noise) {
-        let audio = document.getElementById("clip4");
-        audio.play();
-    }
-    noise = true;
-    yellowPanel.style.backgroundColor = "yellow";
+  if (noise) {
+    let audio = document.getElementById("clip4");
+    audio.play();
+  }
+  noise = true;
+  yellowPanel.style.backgroundColor = "yellow";
 }
 
 function clearColor() {
-    bluePanel.style.backgroundColor = "#0000a5";
-    greenPanel.style.backgroundColor = "#024b30";
-    redPanel.style.backgroundColor = "#a50000";
-    yellowPanel.style.backgroundColor = "#ffcb0c";
+  bluePanel.style.backgroundColor = "#0000a5";
+  greenPanel.style.backgroundColor = "#024b30";
+  redPanel.style.backgroundColor = "#a50000";
+  yellowPanel.style.backgroundColor = "#ffcb0c";
 }

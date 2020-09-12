f575f66dfabb1ccbceaefbf780694833b0a24ce2
MichaelWarner32 <warner.mike32@gmail.com>
2020-09-12 20:56:08 +0000

    Defined variables I will use throughout the coding.

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+let order = [];
+let playerOrder = [];
+let flash;
+let turn;
+let good;
+let compTurn;
+let intervalId;
+let harsh = false;
+let noise = true;
+let on = false;
+let win;
+
+const turnCounter = document.querySelector("#turn");
+const bluePanel = document.querySelector("#bluePanel");
+const greenPanel = document.querySelector("#greenPanel");
+const redPanel = document.querySelector("#redPanel");
+const yellowPanel = document.querySelector("#yellowPanel");
